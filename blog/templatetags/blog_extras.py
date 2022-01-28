08df097e6655a988ea1d55d70a8c202210619ade
Moisés López <moisalas@gmail.com>
2022-01-28 11:53:06 +0000

    Finish crispy forms

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -3,6 +3,7 @@ user_model = get_user_model()
 from django import template
 register = template.Library()
 
+
 from django.utils.html import format_html
 
 @register.filter
@@ -32,8 +33,15 @@ def author_details(author, current_user):
 def row(extra_classes=""):
     return format_html('<div class="row {}">', extra_classes)
 
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
 
 @register.simple_tag
 def endrow():
+    return format_html("</div>")
+
+@register.simple_tag
+def endcol():
     return format_html("</div>")
 
