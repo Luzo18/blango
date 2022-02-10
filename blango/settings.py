297d2761d754e9fefb9f39cdaad59b1ce83b4944
Moisés López <moisalas@gmail.com>
2022-02-10 13:33:42 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -45,7 +45,12 @@ class Dev(Configuration):
   AUTH_USER_MODEL = 'blango_auth.User'
   EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
   ACCOUNT_ACTIVATION_DAYS = 7
-  REGISTRATION_OPEN = False
+  REGISTRATION_OPEN = True
+  SITE_ID = 1
+  ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+  ACCOUNT_EMAIL_REQUIRED = True
+  ACCOUNT_USERNAME_REQUIRED = False
+  ACCOUNT_AUTHENTICATION_METHOD = "email"
   # Application definition
 
   INSTALLED_APPS = [
@@ -54,12 +59,17 @@ class Dev(Configuration):
       'django.contrib.contenttypes',
       'django.contrib.sessions',
       'django.contrib.messages',
+      'django.contrib.sites',
       'django.contrib.staticfiles',
       'blango_auth',
       'blog',
       'crispy_forms',
       'crispy_bootstrap5',
-      'debug_toolbar'
+      'debug_toolbar',
+      'allauth',
+      'allauth.account',
+      'allauth.socialaccount',
+      'allauth.socialaccount.providers.google'
 
   ]
 
